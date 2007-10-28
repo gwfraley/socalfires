bb207a46ba33dc669bfa4a09b9d36b2eff3a60a4
r0010m <r0010m@a4070619-573d-0410-84a2-690469976e6c>
2007-10-28 05:31:40 +0000

    removed irrelevant file

--- trunk/KmlTransformer/resources/xjc.bat
+++
@@ -1,7 +0,0 @@
-C:\Java\SE6\bin\xjc.exe ^
-http://code.google.com/apis/kml/schema/kml22beta.xsd ^
-http://docs.oasis-open.org/election/external/xAL.xsd ^
--b C:\dev\workspace\KmlTransformer\resources\kml.xjb ^
--b C:\dev\workspace\KmlTransformer\resources\oasis.xjb ^
--d C:\dev\workspace\KmlTransformer\src\com\ram6\kml\binding ^
--d C:\dev\workspace\KmlTransformer\src\com\ram6\kml\binding\oasis
